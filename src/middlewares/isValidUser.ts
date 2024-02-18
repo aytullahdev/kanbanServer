9dbc236b07d68d50b8389b98be951a3b6acf8642
ayat-liberatelabs <ayat@liberate-labs.com>
2024-02-18 23:14:54 +0600

    split error handled

--- src/middlewares/isValidUser.ts
+++ src/middlewares/isValidUser.ts
@@ -5,21 +5,25 @@ interface MyPayload {
   email: string;
 }
 export default async function isValidUser(c: any, next: Next) {
-  const token = c.req.header("authorization").split(" ")[1];
-  if (!token) {
+  try {
+    const token = c.req.header("authorization").split(" ")[1];
+    if (!token) {
+      return c.json({ message: "Invalid user!" }, 401);
+    }
+    const isValid = await jwt.verify(token, c.env.JWT_SECRET);
+    if (!isValid) {
+      return c.json({ message: "Invalid user!" }, 401);
+    }
+
+    const { payload } = jwt.decode<MyPayload>(token);
+
+    if (!payload?.email) {
+      return c.json({ message: "Invalid user!" }, 401);
+    }
+
+    c.set("user", payload.email);
+    await next();
+  } catch (error) {
     return c.json({ message: "Invalid user!" }, 401);
   }
-  const isValid = await jwt.verify(token, c.env.JWT_SECRET);
-  if (!isValid) {
-    return c.json({ message: "Invalid user!" }, 401);
-  }
-
-  const { payload } = jwt.decode<MyPayload>(token);
-
-  if (!payload?.email) {
-    return c.json({ message: "Invalid user!" }, 401);
-  }
-
-  c.set("user", payload.email);
-  await next();
 }
